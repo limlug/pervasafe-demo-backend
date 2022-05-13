8ff836ccec38bbe81806c3a537b1cc91e207cf5f
Jonas Pöhler <jonas.poehler@uni-siegen.de>
2022-05-13 16:08:32 +0200

    Added Stress Model

--- app/main.py
+++ app/main.py
@@ -3,6 +3,32 @@ from loguru import logger
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import heartpy as hp
+from sklearn import linear_model
+import pandas as pd
+import numpy as np
+import os
+logger.debug(os.getcwd())
+data_low_stress = pd.read_csv("./app/subject_1_low_stress.csv")
+data_low_stress.columns = {"Time", "Value"}
+i = 0
+low_stress_windowed = []
+while i < len(data_low_stress.values)-1000:
+  low_stress_windowed.append(data_low_stress["Value"].values[i:i+1000])
+  i += 100
+low_stress_label = np.random.randint(low=0, high=30, size=np.array(low_stress_windowed).shape[0])
+data_high_stress = pd.read_csv("./app/subject_1_high_stress.csv")
+data_high_stress.columns = {"Time", "Value"}
+i = 0
+high_stress_windowed = []
+while i < len(data_high_stress.values)-1000:
+  high_stress_windowed.append(data_high_stress["Value"].values[i:i+1000])
+  i += 100
+high_stress_label = np.random.randint(low=60, high=100, size=np.array(high_stress_windowed).shape[0])
+X = np.concatenate((np.array(low_stress_windowed), np.array(high_stress_windowed)), axis=0)
+Y = np.concatenate((low_stress_label, high_stress_label), axis=0)
+stress_index_regressor = linear_model.BayesianRidge()
+stress_index_regressor.fit(X, Y)
+logger.debug("Startup Complete")
 
 app = FastAPI()
 
@@ -24,7 +50,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 data = {}
-
 
 class DataModel(BaseModel):
     request_id: str
@@ -52,11 +77,12 @@ async def get_data(request_id):
         filtered_data = hp.filter_signal(heart_data, cutoff=[0.75, 3.5], sample_rate=100.0, order=3,
                                          filtertype='bandpass')
         working_data, measures = hp.process(filtered_data, sample_rate=100.0)
+        stress_index = stress_index_regressor.predict([heart_data[-1000:]])
         logger.debug(measures)
         return {"heartrate": float(f'{measures["bpm"]:.2f}'),
                 "pnn20": float(f'{measures["pnn20"]:.2f}'),
                 "breathingrate": float(f'{measures["breathingrate"]:.3f}'),
-                "stressindex": 0
+                "stressindex": int(stress_index[0]),
                 }
     else:
         return {"heartrate": 0, "pnn20": 0, "breathingrate": 0, "stressindex": 0}
